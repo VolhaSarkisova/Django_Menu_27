c51467c2f3644ac13aa05d49c692653df0e712a5
VolhaSarkisova <olkavalkiria@gmial.com>
2023-08-29 15:15:28 +0300

    -feat: setting urls of site

---
+++ menu/urls.py
@@ -0,0 +1,9 @@
+
+from django.urls import path
+from menu.views import child, page1, page2
+
+urlpatterns = [
+    path('child/', child, name="child"),
+    path('page1/', page1, name="page1"),
+    path('page2/', page2, name="page2"),
+]
