a9d3218abb972a710f2ce2f0ce96289393f8f753
VolhaSarkisova <olkavalkiria@gmial.com>
2023-08-31 14:50:47 +0300

    -feat: linking

--- menu/urls.py
+++ menu/urls.py
@@ -2,6 +2,8 @@
 from django.urls import path
 from menu.views import child, page1, page2
 
+app_name = "menu"
+
 urlpatterns = [
     path('child/', child, name="child"),
     path('page1/', page1, name="page1"),
